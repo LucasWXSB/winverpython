6c098ccbdb0d125f78c241da5ff607259c22faa4
LucasWXSB <101915910+LucasWXSB@users.noreply.github.com>
2022-12-24 16:45:06 +0800

    Add files via upload

---
+++ 星星/星星.py
@@ -0,0 +1,17 @@
+import turtle
+s=turtle.Pen()
+s.shape("classic")
+s.color("red")
+#画笔进行的操作
+s.forward(225)
+s.right(135)
+s.forward(250)
+s.right(150)
+s.forward(250)
+s.right(150)
+s.forward(250)
+s.right(150)
+s.forward(250)
+s.right(150)
+#The end
+turtle.done()
